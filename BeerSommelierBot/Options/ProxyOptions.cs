f29c2a49a675f3ed7ebafd34951c0290e306e04c
Антон <amarant1503@gmail.com>
2018-08-17 23:33:01 +0300

    application options. proxy options

---
+++ BeerSommelierBot/Options/ProxyOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerSommelierBot.Options
+{
+    public class ProxyOptions
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
